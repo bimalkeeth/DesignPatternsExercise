dfc8d745af703a2d783960c3a5ae09940a0f7e34
vtel.mobicom <bimalkeeth@gmail.com>
2018-08-04 03:01:11 +1000

    Factory Method

--- DesignPatterns/Factory/Factory.go
+++ DesignPatterns/Factory/Factory.go
@@ -1,1 +1,28 @@
 package Factory
+
+import "errors"
+
+const (
+	Cash      = 1
+	DebitCard = 2
+)
+
+type PaymentMethod interface {
+	Pay(amount float64) string
+}
+
+func GetPaymentMethod(m int) (PaymentMethod, error) {
+
+	return nil, errors.New("not implemented yet")
+}
+
+type CashPM struct{}
+type DebitCardPM struct{}
+
+func (c *CashPM) Pay(amount float64) string {
+	return ""
+}
+
+func (c *DebitCardPM) Pay(amount float64) string {
+	return ""
+}
