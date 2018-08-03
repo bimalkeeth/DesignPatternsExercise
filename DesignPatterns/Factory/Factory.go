2ceeeee458db85a1bf34145044590da70da86288
vtel.mobicom <bimalkeeth@gmail.com>
2018-08-04 03:32:27 +1000

    Factory Method

--- DesignPatterns/Factory/Factory.go
+++ DesignPatterns/Factory/Factory.go
@@ -1,6 +1,9 @@
 package Factory
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 const (
 	Cash      = 1
@@ -13,16 +16,23 @@ type PaymentMethod interface {
 
 func GetPaymentMethod(m int) (PaymentMethod, error) {
 
-	return nil, errors.New("not implemented yet")
+	switch m {
+	case Cash:
+		return new(CashPM), nil
+	case DebitCard:
+		return new(DebitCardPM), nil
+	default:
+		return nil, errors.New(fmt.Sprintf("Payment method %d not	recognized\n", m))
+	}
 }
 
 type CashPM struct{}
 type DebitCardPM struct{}
 
 func (c *CashPM) Pay(amount float64) string {
-	return ""
+	return fmt.Sprintf("%0.2f paid using cash\n", amount)
 }
 
 func (c *DebitCardPM) Pay(amount float64) string {
-	return ""
+	return fmt.Sprintf("%#0.2f paid using debit card\n", amount)
 }
