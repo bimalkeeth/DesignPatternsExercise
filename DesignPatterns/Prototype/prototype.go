d1170a9226af06deace4e264de284af2e3a2df15
vtel.mobicom <bimalkeeth@gmail.com>
2018-08-04 18:50:51 +1000

    Abstract Factory Method

---
+++ DesignPatterns/Prototype/prototype.go
@@ -0,0 +1,73 @@
+package Prototype
+
+import (
+	"errors"
+
+	"fmt"
+)
+
+const (
+	White = 1
+	Black = 2
+	Blue  = 3
+)
+
+type ShirtCloner interface {
+	GetClone(s int) (ItemInfoGetter, error)
+}
+
+func GetShirtCloner() ShirtCloner {
+	return &ShirtsCache{}
+}
+
+type ShirtsCache struct{}
+
+func (sc *ShirtsCache) GetClone(s int) (ItemInfoGetter, error) {
+	switch s {
+	case White:
+		newItem := *whitePrototype
+		return &newItem, nil
+	case Black:
+		newItem := *blackPrototype
+		return &newItem, nil
+	case Blue:
+		newItem := *bluePrototype
+		return &newItem, nil
+	default:
+		return nil, errors.New("Shirt model not recognized")
+	}
+}
+
+type ItemInfoGetter interface {
+	GetInfo() string
+}
+type ShirtColor byte
+
+type Shirt struct {
+	Price float32
+	SKU   string
+	Color ShirtColor
+}
+
+func (i *Shirt) GetPrice() float32 {
+	return i.Price
+}
+func (s *Shirt) GetInfo() string {
+	return fmt.Sprintf("Shirt with SKU '%s' and Color id %d that costs	%f\n", s.SKU, s.Color, s.Price)
+}
+
+var whitePrototype *Shirt = &Shirt{
+	Price: 15.00,
+	SKU:   "empty",
+	Color: White,
+}
+var blackPrototype *Shirt = &Shirt{
+	Price: 16.00,
+	SKU:   "empty",
+	Color: Black,
+}
+var bluePrototype *Shirt = &Shirt{
+	Price: 17.00,
+	SKU:   "empty",
+	Color: Blue,
+}
